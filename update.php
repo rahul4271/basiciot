52bfe5dd00f97a9d54d49681cf8515cb23825dcb
Rahul Balabhadruni <51848397+rahul4271@users.noreply.github.com>
2021-01-31 15:06:28 +0530

    Add files via upload

---
+++ update.php
@@ -0,0 +1,1 @@
+<?phpinclude("connect.php");$light=strtoupper($_GET['state']);$id=$_GET['id'];if($light!=""){  $query="UPDATE led SET state='$light' WHERE id=$id";  $data=mysqli_query($conn,$query);  // connection variable, query variable  if($data)  {    // echo $id;    // echo ($light);    }}else {  echo "Parameters empty";} ?>
