30fa4681f7059869ae10e942b807149e872bfcd9
Rahul Balabhadruni <51848397+rahul4271@users.noreply.github.com>
2022-03-25 20:00:51 +0530

    Update status.php

--- status.php
+++ status.php
@@ -1,16 +1,13 @@
 <?php
 include("connect.php");
- $status= $_GET['status'];
-   $queryd="UPDATE time SET status='$status' WHERE id=1";
-   $datad=mysqli_query($conn,$queryd);
+$status= $_GET['status'];
+$queryd="UPDATE time SET status='$status' WHERE id=1";
+$datad=mysqli_query($conn,$queryd);
 $query = "SELECT * FROM led ";
 $data=mysqli_query($conn,$query);
 $numrows = mysqli_num_rows($data);
 if($numrows>0)
-{   
-   
-
-  
+{     
     $querya = "SELECT * FROM led WHERE id=1 ";
     $dataa=mysqli_query($conn,$querya);
     $arraya=mysqli_fetch_assoc($dataa);
@@ -26,20 +23,11 @@ if($numrows>0)
     $arrayc=mysqli_fetch_assoc($datac);
     echo $arrayc['id'];
     echo $arrayc['state'];    
-
 }
 else
 {
-    echo "Error in query or no data";
-    
+    echo "Error in query or no data";    
 }
-// else {
-//      $queryd="UPDATE led SET status=0 WHERE id=1";
-
-//   $datad=mysqli_query($conn,$queryd);
-    
-// }
-
 	
 ?>
 
