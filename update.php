225161fd67c2fc013b2665e637e5f624211b8b0e
Rahul Balabhadruni <51848397+rahul4271@users.noreply.github.com>
2022-03-25 19:58:49 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,1 +1,21 @@
-<?phpinclude("connect.php");$light=strtoupper($_GET['state']);$id=$_GET['id'];if($light!=""){  $query="UPDATE led SET state='$light' WHERE id=$id";  $data=mysqli_query($conn,$query);  // connection variable, query variable  if($data)  {    // echo $id;    // echo ($light);    }}else {  echo "Parameters empty";} ?>
+<?php
+include("connect.php");
+$light=strtoupper($_GET['state']);
+$id=$_GET['id'];
+if($light!="")
+{
+  $query="UPDATE led SET state='$light' WHERE id=$id";
+  $data=mysqli_query($conn,$query);
+  // connection variable, query variable
+  if($data)
+  {
+    // echo $id;
+    // echo ($light);
+  
+  }
+}
+else {
+  echo "Parameters empty";
+}
+
+ ?>
